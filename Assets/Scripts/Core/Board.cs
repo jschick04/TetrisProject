f79326a1da3afc68e62154ef61caa27e8ca306fa
Joseph Schick <jschick04@gmail.com>
2018-05-04 15:10:42 +0000

    Merged PR 5: Added UI elements and background

--- Assets/Scripts/Core/Board.cs
+++ Assets/Scripts/Core/Board.cs
@@ -1,30 +1,37 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Board : MonoBehaviour {
+namespace Core {
 
-  [SerializeField] private Transform _emptySprite;
-  [SerializeField] private int _header = 8;
-  [SerializeField] private int _height = 30;
-  [SerializeField] private int _width = 10;
+  public class Board : MonoBehaviour {
 
-  public void Start() {
-    DrawEmptyCells();
-  }
+    [SerializeField] private Transform _emptySprite;
+    private Transform[,] _grid;
+    [SerializeField] private int _header = 8;
+    [SerializeField] private int _height = 30;
+    [SerializeField] private int _width = 10;
 
-  private void DrawEmptyCells() {
-    if (_emptySprite != null) {
-      for (int y = 0; y < _height - _header; y++) {
-        for (int x = 0; x < _width; x++) {
-          Transform clone = Instantiate(_emptySprite, new Vector3(x, y, 0), Quaternion.identity);
-          clone.name = $"Board Space (x = {x}, y = {y})";
-          clone.transform.parent = transform;
-        }
-      }
-    } else {
-      Debug.LogWarning("Empty Sprite is missing");
+    public void Awake() {
+      _grid = new Transform[_width, _height];
     }
+
+    public void Start() {
+      DrawEmptyCells();
+    }
+
+    private void DrawEmptyCells() {
+      if (_emptySprite != null) {
+        for (int y = 0; y < _height - _header; y++) {
+          for (int x = 0; x < _width; x++) {
+            Transform clone = Instantiate(_emptySprite, new Vector3(x, y, 0), Quaternion.identity);
+            clone.name = $"Board Space (x = {x}, y = {y})";
+            clone.transform.parent = transform;
+          }
+        }
+      } else {
+        Debug.LogWarning("Empty Sprite is missing");
+      }
+    }
+
   }
 
 }
